f4250cd964b645663262c9ff13e89a05a7caaf50
Elias Ylönen <elias.ylonen@aalto.fi>
2018-11-24 11:51:54 +0200

    Display map.

--- ReactProjectX/src/App.js
+++ ReactProjectX/src/App.js
@@ -1,30 +1,22 @@
 import React, { Component } from 'react';
-import styled from 'styled-components';
+import { createGlobalStyle } from 'styled-components'
+import ParkingMap from './components/maps/ParkingMap'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <Text>
-            Edit <code>src/App.js</code> and save to reload.
-          </Text>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="App" >
+        <ParkingMap />
+        <GlobalStyle />
       </div>
     );
   }
 }
 
-const Text = styled.p`
-  font-size: 27px;
-`;
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+  }
+`
 
 export default App;
