51aedaf92f2346ee6d9a0730627fa4207a14f26a
Elias Ylönen <elias.ylonen@aalto.fi>
2018-11-24 17:33:55 +0200

    Heat wip.

--- ReactProjectX/src/components/maps/ParkingMap.js
+++ ReactProjectX/src/components/maps/ParkingMap.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { compose, withProps } from "recompose"
 import { withScriptjs, withGoogleMap, GoogleMap, Polyline, Marker } from "react-google-maps"
 import ParkingSpaces from '../../data/ParkingSpaces'
+import { distanceFromParkingSpace } from '../../tools/distanceFromLine'
 
 const ParkingMap = compose(
   withProps({
@@ -13,21 +14,54 @@ const ParkingMap = compose(
   withScriptjs,
   withGoogleMap
 )((props) => {
+
+  //console.log("heatPolylines", heatPolylines(ParkingSpaces, props.cameraPoints))
+
   return <GoogleMap
     defaultZoom={16}
     defaultCenter={props.center}
   >
-    {props.cameraPoints.map(cameraPoint => {
-      let pos = {
-        lat: cameraPoint.lat,
-        lng: cameraPoint.lng
-      }
-      console.log(pos)
-      return <Marker position={pos} />
-    })}
-    {ParkingSpaces.map(parkingSpace => <Polyline path={parkingSpace} options={{strokeColor: "#FF0000", strokeWeight: 6}} key={JSON.stringify(parkingSpace)} />)}
+    {
+      /*props.cameraPoints.map((cameraPoint, i) => {
+        let pos = {
+          lat: cameraPoint.lat,
+          lng: cameraPoint.lng
+        }
+        return <Marker position={pos} key={i} />
+      })*/
+    }
+    {
+      //ParkingSpaces.map((parkingSpace, i) => <Polyline path={parkingSpace} options={{ strokeColor: "#FF0000", strokeWeight: 6 }} key={/*JSON.stringify(parkingSpace)*/i} />)
+
+      heatPolylines(ParkingSpaces, props.cameraPoints).map((heatPolyline, i) => {
+        let hexHeat = (heatPolyline.heat * 255).toString(16).slice(0, 2)
+        console.log(hexHeat)
+        return <Polyline path={heatPolyline.path} options={{ strokeColor: `#${hexHeat}0000`, strokeWeight: 6 }} key={i} />
+      })
+    }
   </GoogleMap>
 }
 )
+
+function heatPolylines(ParkingSpaces, cameraPoints) {
+  let maxHeat = 0
+
+  return ParkingSpaces.map(parkingSpace => {
+    return {
+      path: parkingSpace,
+      heat: cameraPoints
+        .slice(0, 10)
+        .filter(cameraPoint => cameraPoint.has_car)
+        .map(cameraPoint => distanceFromParkingSpace(cameraPoint, parkingSpace))
+        .reduce((dist1, dist2) => dist1 + dist2, 0)
+    }
+  }).map(heatPolyline => {
+    if (maxHeat < heatPolyline.heat) maxHeat = heatPolyline.heat
+    return heatPolyline
+  }).map(heatPolyline => ({
+    path: heatPolyline.path,
+    heat: heatPolyline.heat / maxHeat
+  }))
+}
 
 export default ParkingMap
--- ReactProjectX/src/services/getCameraPoints.js
+++ ReactProjectX/src/services/getCameraPoints.js
@@ -1,1 +1,6 @@
-export default async () => await (await fetch('http://9d885e4e.ngrok.io/points')).json()
+export default async () => (await (await (await fetch('http://9d885e4e.ngrok.io/points')).json())).map(cameraPoint => ({
+    lat: cameraPoint.latitude,
+    lng: cameraPoint.longitude,
+    has_car: cameraPoint.has_car,
+    timestamp: cameraPoint.timestamp
+}))
