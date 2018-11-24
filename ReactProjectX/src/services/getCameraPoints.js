e11ce5919137d8987968c9de8c6bd744ad2d0fef
Frans L <frans.lehmusvaara@taitounited.fi>
2018-11-24 19:55:33 +0200

    fix: fixed camerapoints

--- ReactProjectX/src/services/getCameraPoints.js
+++ ReactProjectX/src/services/getCameraPoints.js
@@ -1,8 +1,7 @@
-export default async () => {
+export default async () =>
   (await (await fetch('http://9d885e4e.ngrok.io/points')).json()).map(cameraPoint => ({
     lat: cameraPoint.latitude,
     lng: cameraPoint.longitude,
     has_car: cameraPoint.has_car,
     timestamp: cameraPoint.timestamp,
   }));
-};
