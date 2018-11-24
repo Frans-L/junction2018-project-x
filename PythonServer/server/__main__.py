f7ee680dfa0ec95a92cfdaceab4b6850c475e920
Joonatan Bergholm <joonatan.bergholm@taitounited.fi>
2018-11-24 14:08:59 +0200

    Added endpoints

--- PythonServer/server/__main__.py
+++ PythonServer/server/__main__.py
@@ -6,6 +6,8 @@ import tensorflow as tf
 
 import cv2
 import numpy as np
+from time import time
+import json
 from matplotlib import pyplot as plt
 
 sess = tf.Session()
@@ -19,26 +21,73 @@ tf_train = graph.get_tensor_by_name("train_input:0")
 tf_label = graph.get_tensor_by_name("labels_input:0")
 sm = graph.get_tensor_by_name("output:0")
 
+points = [
+    {
+        'lat': 60.22,
+        'lon': 22.43,
+        'timestamp': time(),
+        'has_car': True,
+    },
+    {
+        'lat': 60.23,
+        'lon': 22.44,
+        'timestamp': time(),
+        'has_car': True,
+    },
+    {
+        'lat': 60.25,
+        'lon': 22.43,
+        'timestamp': time(),
+        'has_car': True,
+    },
+    {
+        'lat': 60.25,
+        'lon': 22.43,
+        'timestamp': time(),
+        'has_car': False,
+    },
+]
+
 @route('/img', method=['POST'])
 def img():
+    global points
+
     file = next(request.files.values()).file
     data = np.asarray(bytearray(file.read()), np.uint8)
     orig = cv2.cvtColor(cv2.imdecode(data, cv2.CV_32S), cv2.COLOR_BGR2RGB)
 
-    #w, h, _ = orig.shape
-    #print(int(w/3), int(-w/3), int(h/3), int(-h/3))
-    img = orig[:, :] #[int(w/3):int(-w/3), int(h/3):int(-h/3)]
+    w, h, _ = orig.shape
+    img = orig[int(w/3):int(-w/3), int(h/3):int(-h/3)]
 
     img = sess.run([sm], feed_dict={tf_data: img.reshape([1, *img.shape]), tf_train: False})
 
     img = np.array(img)
 
-    plt.subplot(211)
-    plt.imshow(orig)
-    plt.subplot(212)
-    plt.imshow(img[0,0,:,:,0])
+    has_car = np.average(img) > 0.5
 
-    plt.show()
+    print(np.max(img), np.min(img), np.average(img))
+
+    data = {
+      **request.json,
+      'timestamp': time(),
+      'has_car': has_car,
+    }
+
+    points.append(data)
+
+    return json.dumps(data)
+
+    # plt.subplot(211)
+    # plt.imshow(orig)
+    # plt.subplot(212)
+    # plt.imshow(img[0,0,:,:,0])
+
+    # plt.show()
+
+@route('/points', method=['GET'])
+def point():
+    return json.dumps(points)
+
 
 if __name__ == '__main__':
     try:
