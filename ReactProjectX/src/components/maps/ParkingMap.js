cecb7f85ad85a7a2492baaedf72889fc3ba4cfdc
Frans L <frans.lehmusvaara@taitounited.fi>
2018-11-24 19:20:14 +0200

    fix: fixed linting errors

--- ReactProjectX/src/components/maps/ParkingMap.js
+++ ReactProjectX/src/components/maps/ParkingMap.js
@@ -1,66 +1,69 @@
-import React from 'react'
-import { compose, withProps } from "recompose"
-import { withScriptjs, withGoogleMap, GoogleMap, Polyline, Marker } from "react-google-maps"
-import ParkingSpaces from '../../data/ParkingSpaces'
-import { distanceFromParkingSpace } from '../../tools/distanceFromLine'
+import React from 'react';
+import { compose, withProps } from 'recompose';
+import { withScriptjs, withGoogleMap, GoogleMap, Polyline, Marker } from 'react-google-maps';
+import parkingSpaces from '../../data/ParkingSpaces';
+import { distanceFromParkingSpace } from '../../tools/distanceFromLine';
 
 const ParkingMap = compose(
   withProps({
-    googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyCe_M1YhbssjRfg9jerKmpMn38jJY6TmT0&v=3.exp&libraries=geometry,drawing,places",
-    loadingElement: <div style={{ height: `100%` }} />,
-    containerElement: <div style={{ height: `100vh` }} />,
-    mapElement: <div style={{ height: `100%` }} />
+    googleMapURL:
+      'https://maps.googleapis.com/maps/api/js?key=AIzaSyCe_M1YhbssjRfg9jerKmpMn38jJY6TmT0&v=3.exp&libraries=geometry,drawing,places',
+    loadingElement: <div style={{ height: '100%' }} />,
+    containerElement: <div style={{ height: '100vh' }} />,
+    mapElement: <div style={{ height: '100%' }} />,
   }),
   withScriptjs,
-  withGoogleMap
-)((props) => {
+  withGoogleMap,
+)(props => {
+  // console.log("heatPolylines", heatPolylines(parkingSpaces, props.cameraPoints))
 
-  //console.log("heatPolylines", heatPolylines(ParkingSpaces, props.cameraPoints))
-
-  return <GoogleMap
-    defaultZoom={16}
-    defaultCenter={props.center}
-  >
-    {
-      /*props.cameraPoints.map((cameraPoint, i) => {
+  return (
+    <GoogleMap defaultZoom={16} defaultCenter={props.center}>
+      {/*props.cameraPoints.map((cameraPoint, i) => {
         let pos = {
           lat: cameraPoint.lat,
           lng: cameraPoint.lng
         }
         return <Marker position={pos} key={i} />
-      })*/
-    }
-    {
-      //ParkingSpaces.map((parkingSpace, i) => <Polyline path={parkingSpace} options={{ strokeColor: "#FF0000", strokeWeight: 6 }} key={/*JSON.stringify(parkingSpace)*/i} />)
+      }) */}
+      {//parkingSpaces.map((parkingSpace, i) => <Polyline path={parkingSpace} options={{ strokeColor: "#FF0000", strokeWeight: 6 }} key={/*JSON.stringify(parkingSpace)*/i} />)
 
-      heatPolylines(ParkingSpaces, props.cameraPoints).map((heatPolyline, i) => {
-        let hexHeat = (heatPolyline.heat * 255).toString(16).slice(0, 2)
-        console.log(hexHeat)
-        return <Polyline path={heatPolyline.path} options={{ strokeColor: `#${hexHeat}0000`, strokeWeight: 8 }} key={i} />
-      })
-    }
-  </GoogleMap>
+      heatPolylines(parkingSpaces, props.cameraPoints).map((heatPolyline, i) => {
+        const hexHeat = (heatPolyline.heat * 255).toString(16).slice(0, 2);
+        console.log(hexHeat);
+        return (
+          <Polyline
+            path={heatPolyline.path}
+            options={{ strokeColor: `#${hexHeat}0000`, strokeWeight: 8 }}
+            key={i}
+          />
+        );
+      })}
+    </GoogleMap>
+  );
+});
+
+function heatPolylines(parkingSpaces, cameraPoints) {
+  let maxHeat = 0;
+
+  return parkingSpaces
+    .map(parkingSpace => {
+      return {
+        path: parkingSpace,
+        heat: cameraPoints
+          .filter(cameraPoint => cameraPoint.has_car)
+          .map(cameraPoint => 1 / distanceFromParkingSpace(cameraPoint, parkingSpace))
+          .reduce((dist1, dist2) => dist1 + dist2, 0),
+      };
+    })
+    .map(heatPolyline => {
+      if (maxHeat < heatPolyline.heat) maxHeat = heatPolyline.heat;
+      return heatPolyline;
+    })
+    .map(heatPolyline => ({
+      path: heatPolyline.path,
+      heat: heatPolyline.heat / maxHeat,
+    }));
 }
-)
 
-function heatPolylines(ParkingSpaces, cameraPoints) {
-  let maxHeat = 0
-
-  return ParkingSpaces.map(parkingSpace => {
-    return {
-      path: parkingSpace,
-      heat: cameraPoints
-        .filter(cameraPoint => cameraPoint.has_car)
-        .map(cameraPoint => 1 / distanceFromParkingSpace(cameraPoint, parkingSpace))
-        .reduce((dist1, dist2) => dist1 + dist2, 0)
-    }
-  }).map(heatPolyline => {
-    if (maxHeat < heatPolyline.heat) maxHeat = heatPolyline.heat
-    return heatPolyline
-  }).map(heatPolyline => ({
-    path: heatPolyline.path,
-    heat: heatPolyline.heat / maxHeat
-  }))
-}
-
-export default ParkingMap
+export default ParkingMap;
