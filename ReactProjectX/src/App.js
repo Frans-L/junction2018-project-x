d98dc568c57df7bee7c3ce49ac5d062905866d26
Elias Ylönen <elias.ylonen@aalto.fi>
2018-11-24 13:08:39 +0200

    Add geolocation.

--- ReactProjectX/src/App.js
+++ ReactProjectX/src/App.js
@@ -3,13 +3,33 @@ import { createGlobalStyle } from 'styled-components'
 import ParkingMap from './components/maps/ParkingMap'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      center: undefined
+    }
+  }
+
   render() {
     return (
       <div className="App" >
-        <ParkingMap />
+        <ParkingMap center={this.state.center} />
         <GlobalStyle />
       </div>
     );
+  }
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(position => {
+      console.log(position)
+      let mapsLoc = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      }
+      this.setState({
+        center: mapsLoc
+      })
+    })
   }
 }
 
--- ReactProjectX/src/components/maps/ParkingMap.js
+++ ReactProjectX/src/components/maps/ParkingMap.js
@@ -1,23 +1,24 @@
 import React from 'react'
 import { compose, withProps } from "recompose"
-import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
+import { withScriptjs, withGoogleMap, GoogleMap, Polyline } from "react-google-maps"
 
 const ParkingMap = compose(
   withProps({
     googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyCe_M1YhbssjRfg9jerKmpMn38jJY6TmT0&v=3.exp&libraries=geometry,drawing,places",
     loadingElement: <div style={{ height: `100%` }} />,
     containerElement: <div style={{ height: `100vh` }} />,
-    mapElement: <div style={{ height: `100%` }} />,
+    mapElement: <div style={{ height: `100%` }} />
   }),
   withScriptjs,
   withGoogleMap
-)((props) =>
-  <GoogleMap
-    defaultZoom={8}
-    defaultCenter={{ lat: -34.397, lng: 150.644 }}
+)((props) => {
+  return <GoogleMap
+    defaultZoom={16}
+    defaultCenter={props.center}
   >
-    {props.isMarkerShown && <Marker position={{ lat: -34.397, lng: 150.644 }} />}
+    <Polyline path={[{ lat: -34.397, lng: 150.644 }, { lat: -35.397, lng: 149.644 }]} />
   </GoogleMap>
+}
 )
 
 export default ParkingMap
