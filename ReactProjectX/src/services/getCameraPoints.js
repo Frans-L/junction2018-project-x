631571c20a3a4ff68b847401cb6f1968badb7d68
Elias Ylönen <elias.ylonen@aalto.fi>
2018-11-25 02:42:55 +0200

    Use all cameraPoints.

--- ReactProjectX/src/services/getCameraPoints.js
+++ ReactProjectX/src/services/getCameraPoints.js
@@ -1,9 +1,7 @@
 export default async () =>
-  (await (await fetch('http://9d344f40.ngrok.io/points')).json())
-    .map(cameraPoint => ({
-      lat: cameraPoint.latitude,
-      lng: cameraPoint.longitude,
-      weight: cameraPoint.cars.length,
-      timestamp: cameraPoint.timestamp,
-    }))
-    .slice(0, 10);
+  (await (await fetch('http://9d344f40.ngrok.io/points')).json()).map(cameraPoint => ({
+    lat: cameraPoint.latitude,
+    lng: cameraPoint.longitude,
+    weight: cameraPoint.cars.length,
+    timestamp: cameraPoint.timestamp,
+  }));
